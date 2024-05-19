f7ac59127af04b84f880289599ac695dc1ec2391
Sarah El Fraihi <sarah.el.fraihi@student.ehb.be>
2024-05-19 19:00:14 +0200

    fetch

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 function init() {
-    console.log("Let's start coding!");
+	fetch("https://swapi.dev/api/people/1/")
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (RawData) {
+			RawData.forEach(function (Data) {
+				console.log(Data);
+			});
+		});
 }
 
-init()
+init();
