3cbe840d20c0c1b55a3bde28792d958fda88897b
Sarah El Fraihi <sarah.el.fraihi@student.ehb.be>
2024-05-20 15:08:41 +0200

    FetchPeople aan het aanpassen

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
 function init() {
-	fetch("https://swapi.dev/api/people/1/")
+	FetchPeople();
+}
+
+function FetchPeople() {
+	fetch("https://swapi.dev/api/people/")
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (RawData) {
-			RawData.forEach(function (Data) {
+			console.log(RawData);
+			RawData.results.forEach(function (Data) {
 				console.log(Data);
+				displayData(Data);
 			});
 		});
+}
+
+function displayData(Data) {
+	Data.results.forEach((item) => {
+		output += `
+	<h4>${item.name}</h4>`;
+	});
 }
 
 init();
