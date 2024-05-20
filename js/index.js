08c0f36b13c90cd7e42eb042849eb718570b501a
Sarah El Fraihi <sarah.el.fraihi@student.ehb.be>
2024-05-20 17:37:30 +0200

    Species toegevoegs, See more button toe gevoegd en filteren

--- js/index.js
+++ js/index.js
@@ -1,15 +1,51 @@
+let currentCategory = "People";
+let currentPage = 1;
+
 function init() {
-	FetchPeople();
-	FetchPlanet();
+	FetchAllData();
+
+	document.getElementById("People").addEventListener("click", () => {
+		clearContainer();
+		currentCategory = "People";
+		FetchPeople(1);
+	});
+	document.getElementById("Planets").addEventListener("click", () => {
+		clearContainer();
+		currentCategory = "Planets";
+		FetchPlanet(1);
+	});
+	document.getElementById("Films").addEventListener("click", () => {
+		clearContainer();
+		currentCategory = "Films";
+		FetchFilms(1);
+	});
+
+	document.getElementById("seeMore").addEventListener("click", () => {
+		if (currentCategory === "People") {
+			FetchPeople(currentPage + 1);
+		} else if (currentCategory === "Planets") {
+			FetchPlanet(currentPage + 1);
+		} else if (currentCategory === "Films") {
+			FetchFilms(currentPage + 1);
+		}
+	});
 }
 
-function FetchPeople() {
-	fetch("https://swapi.dev/api/people/")
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (RawData) {
-			RawData.results.forEach(function (Data) {
+function FetchAllData() {
+	FetchPeople(1, false);
+	FetchPlanet(1, false);
+	FetchFilms(1, false);
+}
+
+function clearContainer() {
+	document.querySelector("#container").innerHTML = "";
+}
+
+function FetchPeople(page) {
+	fetch(`https://swapi.dev/api/people/?page=${page}`)
+		.then((response) => response.json())
+		.then((RawData) => {
+			RawData.results.forEach((Data) => {
 				const name = Data.name;
 				const height = Data.height;
 				const mass = Data.mass;
@@ -19,29 +55,27 @@ function FetchPeople() {
 				const gender = Data.gender;
 				DisplayPeople(name, height, mass, hair, eye, birth, gender);
 			});
+			currentPage = page;
 		});
 }
 
 function DisplayPeople(name, height, mass, hair, eye, birth, gender) {
-	const html = ` <div class="box"><h3>${name}</h3>
-	<p> height : ${height} cm</p>
-	<p> mass : ${mass} kg</p>
-	<p> hair color : ${hair}</p>
-	<p> eye color : ${eye}</p>
-	<p> birth year : ${birth}</p>
-	<p>	gender : ${gender}</p>
-	</div> 
-	`;
+	const html = `<div class="box"><h3>${name}</h3>
+    <p> height : ${height} cm</p>
+    <p> mass : ${mass} kg</p>
+    <p> hair color : ${hair}</p>
+    <p> eye color : ${eye}</p>
+    <p> birth year : ${birth}</p>
+    <p> gender : ${gender}</p>
+    </div>`;
 	document.querySelector("#container").innerHTML += html;
 }
 
-function FetchPlanet() {
-	fetch("https://swapi.dev/api/planets/")
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (RawData) {
-			RawData.results.forEach(function (Data) {
+function FetchPlanet(page) {
+	fetch(`https://swapi.dev/api/planets/?page=${page}`)
+		.then((response) => response.json())
+		.then((RawData) => {
+			RawData.results.forEach((Data) => {
 				const name = Data.name;
 				const rotation = Data.rotation_period;
 				const orbital = Data.orbital_period;
@@ -61,6 +95,7 @@ function FetchPlanet() {
 					population
 				);
 			});
+			currentPage = page;
 		});
 }
 
@@ -74,17 +109,91 @@ function DisplayPlanet(
 	terrain,
 	population
 ) {
-	const html = ` <div class="box"><h3>${name}</h3>
-	<p> rotation period : ${rotation} cm</p>
-	<p> orbital period : ${orbital} kg</p>
-	<p> diameter : ${diameter}</p>
-	<p> climate : ${climate}</p>
-	<p> gravity : ${gravity}</p>
-	<p>	terrain : ${terrain}</p>
-	<p>	terrain : ${population}</p>
-	</div> 
-	`;
+	const html = `<div class="box"><h3>${name}</h3>
+    <p> rotation period : ${rotation}</p>
+    <p> orbital period : ${orbital}</p>
+    <p> diameter : ${diameter}</p>
+    <p> climate : ${climate}</p>
+    <p> gravity : ${gravity}</p>
+    <p> terrain : ${terrain}</p>
+    <p> population : ${population}</p>
+    </div>`;
 	document.querySelector("#container").innerHTML += html;
 }
 
+function FetchFilms(page) {
+	fetch(`https://swapi.dev/api/films/?page=${page}`)
+		.then((response) => response.json())
+		.then((RawData) => {
+			RawData.results.forEach((Data) => {
+				const title = Data.title;
+				const director = Data.director;
+				const producer = Data.producer;
+				const date = Data.release_date;
+				DisplayFilms(title, director, producer, date);
+			});
+			currentPage = page;
+		});
+}
+
+function DisplayFilms(title, director, producer, date) {
+	const html = `<div class="box"><h3>${title}</h3>
+    <p> director : ${director}</p>
+    <p> producer : ${producer}</p>
+    <p> date : ${date}</p>
+    </div>`;
+	document.querySelector("#container").innerHTML += html;
+}
+
+function FetchSpecies(page) {
+	fetch(`https://swapi.dev/api/species/?page=${page}`)
+		.then((response) => response.json())
+		.then((RawData) => {
+			RawData.results.forEach((Data) => {
+				const names = Data.name;
+				const classification = Data.classification;
+				const designation = Data.designation;
+				const height = Data.average_height;
+				const skin = Data.skin_colors;
+				const hair = Data.hair_colors;
+				const eye = Data.eye_colors;
+				const life = Data.average_lifespan;
+				DisplaySpecies(
+					names,
+					classification,
+					designation,
+					height,
+					skin,
+					hair,
+					eye,
+					life
+				);
+			});
+			currentPage = page;
+		});
+}
+
+function DisplaySpecies(
+	names,
+	classification,
+	designation,
+	height,
+	skin,
+	hair,
+	eye,
+	life
+) {
+	const html = `<div class="box"><h3>${names}</h3>
+    <p> classification : ${classification}</p>
+    <p> designation : ${designation}</p>
+    <p> average height : ${height}</p>
+	<p> skin colors : ${skin}</p>
+	<p> haire colors : ${hair}</p>
+	<p> eye colors : ${eye}</p>
+	<p> life span : ${life}</p>
+    </div>`;
+	document.querySelector("#container").innerHTML += html;
+}
+
+// Initialize the script
 init();
